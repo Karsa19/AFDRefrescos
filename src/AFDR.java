77b05b3f774a141f4f46872f547c9004274796af
Karen <cam.pfer019@gmail.com>
2018-09-08 20:29:45 -0500

    Sin errores en la compra

--- src/AFDR.java
+++ src/AFDR.java
@@ -1,421 +1,335 @@
 import java.util.ArrayList;
 
 public class AFDR {
-	
-	ArrayList<Character> monedas= new ArrayList<Character>();
-	int i=0;
-	private boolean acepta= false;
+
+	ArrayList<Character> monedas = new ArrayList<Character>();
+	int i = 0;
+	int y=0;
+	private boolean acepta = false;
 	private int cambio = 0;
-	
-	
+
 	public void eQ0() {
-		//Estado Inicial
 		
-		if(monedas.size()>i) {
 		
-			if(monedas.get(i)=='1') {
-			i++;
-			eQ1();
-			}
-		
-			if(monedas.get(i)=='2') {
-			i++;
-			eQ2();
-			}
-		
-			if(monedas.get(i)=='5') {
-			i++;
-			eQ5();
-			}
-		
-			if(monedas.get(i)=='D') {
-			i++;
-			eQ10();
-			}
-		
-		}
+		if (monedas.size() >= (i+1)) {
+			if (monedas.get(i) == '1')
+				eQ1();
 
-	}
-	
-	public void eQ1() {
-		//Estado q1
-		
-		if(monedas.size()>=i) {
-			if(monedas.get(i)=='1') {
-				i++;
+			else if (monedas.get(i) == '2')
 				eQ2();
-			}
-			
-			if(monedas.get(i)=='2') {
-				i++;
-				eQ3();
-			}
-			
-			if(monedas.get(i)=='5') {
-				i++;
-				eQ6();
-			}
-			
-			if(monedas.get(i)=='D') {
-				i++;
-				eQ11();
-			}
-		}
 
+			else if (monedas.get(i) == '5')
+				eQ5();
+
+			else if (monedas.get(i) == 'D')
+				eQ10();
+
+		} else
+			acepta = false;
 	}
-	
-	public void eQ2() {
-		//Estado q2
+
+	public void eQ1() {
 		
-		if(monedas.size()>=i) {
-			if(monedas.get(i)=='1') {
-				i++;
+		if (monedas.size() >= (i+1)) {
+			i++;
+			if (monedas.get(i) == '1')
+				eQ2();
+
+			else if (monedas.get(i) == '2')
 				eQ3();
-			}
-			
-			if(monedas.get(i)=='2') {
-				i++;
-				eQ4();
-			}
-			
-			if(monedas.get(i)=='5') {
-				i++;
-				eQ7();
-			}
-			
-			if(monedas.get(i)=='D') {
-				i++;
-				eQ12();
-			}
-		}
-	}
-	
-	public void eQ3() {
-		//Estado q3
-		if(monedas.size()>=i) {
-			if(monedas.get(i)=='1') {
-				i++;
-				eQ4();
-			}
-			
-			if(monedas.get(i)=='2') {
-				i++;
-				eQ5();
-			}
-			
-			if(monedas.get(i)=='5') {
-				i++;
-				eQ8();
-			}
-			
-			if(monedas.get(i)=='D') {
-				i++;
-				eQ13();
-			}
-		}
-	}
-	
-	public void eQ4() {
-		//Estado q4
-		
-		if(monedas.size()>=i) {
-			if(monedas.get(i)=='1') {
-				i++;
-				eQ5();
-			}
-			
-			if(monedas.get(i)=='2') {
-				i++;
+
+			else if (monedas.get(i) == '5')
 				eQ6();
-			}
-			
-			if(monedas.get(i)=='5') {
-				i++;
-				eQ9();
-			}
-			
-			if(monedas.get(i)=='D') {
-				i++;
-				eQ14();
-			}
-		}
-		
+
+			else if (monedas.get(i) == 'D')
+				eQ11();
+		} else
+			acepta = false;
 	}
-	
+
+	public void eQ2() {
+		
+		if (monedas.size() >= (i+1)) {
+			i++;
+			if (monedas.get(i) == '1')
+				eQ3();
+
+			else if (monedas.get(i) == '2')
+				eQ4();
+
+			else if (monedas.get(i) == '5')
+				eQ7();
+
+			else if (monedas.get(i) == 'D')
+				eQ12();
+
+		} else
+			acepta = false;
+	}
+
+	public void eQ3() {
+		
+		if (monedas.size() >= (i+1)) {
+			i++;
+			if (monedas.get(i) == '1')
+				eQ4();
+
+			else if (monedas.get(i) == '2')
+				eQ5();
+
+			else if (monedas.get(i) == '5')
+				eQ8();
+
+			else if (monedas.get(i) == 'D')
+				eQ13();
+		} else
+			acepta = false;
+	}
+
+	public void eQ4() {
+		
+		if (monedas.size() >= (i+1)) {
+			i++;
+			if (monedas.get(i) == '1')
+				eQ5();
+
+			else if (monedas.get(i) == '2')
+				eQ6();
+
+			else if (monedas.get(i) == '5')
+				eQ9();
+
+			else if (monedas.get(i) == 'D')
+				eQ14();
+		} else
+			acepta = false;
+	}
+
 	public void eQ5() {
 		
-		if(monedas.size()>=i) {
-			if(monedas.get(i)=='1') {
-				i++;
+		if (monedas.size() >= (i+1)) {
+			i++;
+			if (monedas.get(i) == '1')
 				eQ6();
-			}
-			
-			if(monedas.get(i)=='2') {
-				i++;
+
+			else if (monedas.get(i) == '2')
 				eQ7();
-			}
-			
-			if(monedas.get(i)=='5') {
-				i++;
+
+			else if (monedas.get(i) == '5')
 				eQ10();
-			}
 
-			if(monedas.get(i)=='D') {
+			else if (monedas.get(i) == 'D')
 				eQ15();
-		
-			}
-		}
 
+		} else
+			acepta = false;
 	}
-	
+
 	public void eQ6() {
-		//Estado q6
 		
-		if(monedas.size()>=i) {
-			if(monedas.get(i)=='1') {
-				i++;
+		if (monedas.size() >= (i+1)) {
+			i++;
+			if (monedas.get(i) == '1')
 				eQ7();
-			}
-			
-			if(monedas.get(i)=='2') {
-				i++;
-				eQ8();
-			}
-			
-			if(monedas.get(i)=='5') {
-				i++;
-				eQ11();
-			}
-			
-			if(monedas.get(i)=='D') {
-				eQ16(monedas.get(i),6);
-			}
-		}
 
+			else if (monedas.get(i) == '2')
+				eQ8();
+
+			else if (monedas.get(i) == '5')
+				eQ11();
+
+			else if (monedas.get(i) == 'D')
+				eQ16();
+		} else
+			acepta = false;
 	}
-	
+
 	public void eQ7() {
-		//Estado q7
 		
-		if(monedas.size()>=i) {
-			if(monedas.get(i)=='1') {
-				i++;
+		if (monedas.size() >= (i+1)) {
+			i++;
+			if (monedas.get(i) == '1')
 				eQ8();
-			}
-			
-			if(monedas.get(i)=='2') {
-				i++;
+
+			else if (monedas.get(i) == '2')
 				eQ9();
-			}
-			
-			if(monedas.get(i)=='5') {
-				i++;
+
+			else if (monedas.get(i) == '5')
 				eQ12();
-			}
-			if(monedas.get(i)=='D') {
-				eQ16(monedas.get(i),7);
-			}
+
+			else if (monedas.get(i) == 'D')
+				eQ16();
 		}
 
+		else
+			acepta = false;
 	}
-	
+
 	public void eQ8() {
-		//Estado q8
 		
-		if(monedas.size()>=i) {
-			if(monedas.get(i)=='1') {
-				i++;
+		if (monedas.size() >= (i+1)) {
+			i++;
+			if (monedas.get(i) == '1')
 				eQ9();
-			}
-			
-			if(monedas.get(i)=='2') {
-				i++;
+
+			else if (monedas.get(i) == '2')
 				eQ10();
-			}
-			
-			if(monedas.get(i)=='5') {
-				i++;
+
+			else if (monedas.get(i) == '5')
 				eQ13();
-			}
-			if(monedas.get(i)=='D') {
-				eQ16(monedas.get(i),8);
-			}
-		}
-		
-	}
-	
-	public void eQ9() {
-		//Estado q9
-		
-		if(monedas.size()>=i) {
-			if(monedas.get(i)=='1') {
-				i++;
-				eQ10();
-			}
-			
-			if(monedas.get(i)=='2') {
-				i++;
-				eQ11();
-			}
-			
-			if(monedas.get(i)=='5') {
-				i++;
-				eQ14();
-			}
-			if(monedas.get(i)=='D') {
-				eQ16(monedas.get(i),9);
-			}
-		}
-		
-	}
-	
-	public void eQ10() {
-		//Estado q10
-		
-		if(monedas.size()>=i) {
-			if(monedas.get(i)=='1') {
-				i++;
-				eQ11();
-			}
-			
-			if(monedas.get(i)=='2') {
-				i++;
-				eQ12();
-			}
-			
-			if(monedas.get(i)=='5') {
-				eQ15();
-			}
-				
-			if(monedas.get(i)=='D') {
-				eQ16(monedas.get(i),10);
-			}
-		}
-		
-	}
-	
-	public void eQ11() {
-		//Estado q11
-		
-		if(monedas.size()>=i) {
-			if(monedas.get(i)=='1') {
-				i++;
-				eQ12();
-			}
-			
-			if(monedas.get(i)=='2') {
-				i++;
-				eQ13();
-			}
-			if(monedas.get(i)=='5') {
-				eQ16(monedas.get(i),11);
-			}
-				
-				
-			if(monedas.get(i)=='D') {
-				eQ16(monedas.get(i),11);
-			}
-		}
-		
-	}
-	
-	public void eQ12() {
-		//Estado q12
-		
-		if(monedas.size()>=i) {
-			if(monedas.get(i)=='1') {
-				i++;
-				eQ13();
-			}
-			
-			if(monedas.get(i)=='2') {
-				i++;
-				eQ14();
-			}
-			if(monedas.get(i)=='5') {
-				eQ16(monedas.get(i),12);
-			}
-			
-			if(monedas.get(i)=='D') {
-				eQ16(monedas.get(i),12);
-			}
+
+			else if (monedas.get(i) == 'D')
+				eQ16();
 		}
 
+		else
+			acepta = false;
 	}
-	
-	public void eQ13() {
-		//Estado q13
+
+	public void eQ9() {
 		
-		if(monedas.size()>=i) {
-			if(monedas.get(i)=='1') {
-				i++;
+		if (monedas.size() >= (i+1)) {
+			i++;
+			if (monedas.get(i) == '1')
+				eQ10();
+
+			else if (monedas.get(i) == '2')
+				eQ11();
+
+			else if (monedas.get(i) == '5')
 				eQ14();
-			}
-			if(monedas.get(i)=='2') {
+
+			else if (monedas.get(i) == 'D')
+				eQ16();
+
+		} else
+			acepta = false;
+	}
+
+	public void eQ10() {
+		
+		if (monedas.size() >= (i+1)) {
+			i++;
+			if (monedas.get(i) == '1')
+				eQ11();
+
+			else if (monedas.get(i) == '2')
+				eQ12();
+
+			else if (monedas.get(i) == '5')
 				eQ15();
-			}
-			
-			if(monedas.get(i)=='5') {
-				eQ16(monedas.get(i),13);
-			}
-			
-			if(monedas.get(i)=='D') {
-				eQ16(monedas.get(i),13);
-			}
-		}
-		
+
+			else if (monedas.get(i) == 'D')
+				eQ16();
+
+		} else
+			acepta = false;
 	}
-	
+
+	public void eQ11() {
+		
+		if (monedas.size() >= (i+1)) {
+			i++;
+			if (monedas.get(i) == '1')
+				eQ12();
+
+			else if (monedas.get(i) == '2')
+				eQ13();
+
+			else if (monedas.get(i) == '5')
+				eQ16();
+
+			else if (monedas.get(i) == 'D')
+				eQ16();
+
+		}
+
+		else
+			acepta = false;
+	}
+
+	public void eQ12() {
+		
+		if (monedas.size() >= (i+1)) {
+			i++;
+			if (monedas.get(i) == '1')
+				eQ13();
+
+			else if (monedas.get(i) == '2')
+				eQ14();
+
+			else if (monedas.get(i) == '5')
+				eQ16();
+
+			else if (monedas.get(i) == 'D')
+				eQ16();
+		} else
+			acepta = false;
+	}
+
+	public void eQ13() {
+		
+		if (monedas.size() >= (i+1)) {
+			i++;
+			if (monedas.get(i) == '1')
+				eQ14();
+
+			else if (monedas.get(i) == '2')
+				eQ15();
+
+			else if (monedas.get(i) == '5')
+				eQ16();
+
+			else if (monedas.get(i) == 'D')
+				eQ16();
+		} else
+			acepta = false;
+	}
+
 	public void eQ14() {
-		//Estado q14
 		
-		if(monedas.get(i)=='1') {
-			eQ15();
+		if (monedas.size() >= (i+1)) {
+			i++;
+			if (monedas.get(i) == '1')
+				eQ15();
+
+			else if (monedas.get(i) == '2')
+				eQ16();
+
+			else if (monedas.get(i) == '5')
+				eQ16();
+
+			else if (monedas.get(i) == 'D')
+				eQ16();
+
 		}
-		
-		if(monedas.get(i)=='2') {
-			eQ16(monedas.get(i),14);
-		}
-		
-		if(monedas.get(i)=='5') {
-			eQ16(monedas.get(i),14);
-		}
-		
-		if(monedas.get(i)=='D') {
-			eQ16(monedas.get(i),14);
-		}
-		
+
+		else
+			acepta = false;
+
 	}
-	
+
 	public void eQ15() {
-		//Estado Final
-		acepta=true;
-		cambio=0;
-	}
 	
-	public void eQ16(char m, int numE) {
-		//Estado error
-		
-		acepta=true;
-		
-		if(m=='1') {
-			cambio=(numE+1)-15;
+		cambio=0;
+		acepta = true;
+		System.out.println(acepta);
+	}
+
+	public void eQ16() {
+		// Estado error
+       int suma,g;
+       suma=0;
+		acepta = true;
+		//Se recorre el arreglo para obtener el total de dinero_
+		//ingresado y asi regresar el cambio
+		for (g=0; g<monedas.size();g++) {
+		if (monedas.get(g) == 'D')
+			suma=suma+10;
+		else
+			suma = suma+Character.getNumericValue(monedas.get(g));
 		}
-		
-		if(m=='2') {
-			cambio=(numE+2)-15;
-		}
-		
-		if(m=='5') {
-			cambio=(numE+5)-15;
-		}
-		
-		if(m=='D') {
-			cambio=(numE+10)-15;
-		}
-		
-		System.out.println("cambio:" + cambio);
-		
+		cambio= suma-15;
 	}
 
 	public boolean getAcepta() {
@@ -425,6 +339,5 @@ public class AFDR {
 	public int getCambio() {
 		return cambio;
 	}
-	
 
 }
